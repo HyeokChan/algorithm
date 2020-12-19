0ba6889e5fd58e9aefb868344ba44cc6de44e831
HyeokChan <48059565+HyeokChan@users.noreply.github.com>
2020-12-19 21:43:01 +0900

    greedy42 컵라면

---
+++ greed/beakjoon1781.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+n = int(sys.stdin.readline().rstrip("\n"))
+nums=[]
+temps=[0]*(n+1)
+for _ in range(n):
+    deadline, value = map(int, sys.stdin.readline().rstrip("\n").split())
+    nums.append((deadline,value))
+
+nums = sorted(nums)
+heaps=[]
+
+for num in nums:
+    heapq.heappush(heaps, num[1])
+    if(num[0] < len(heaps)):
+        heapq.heappop(heaps)
+print(sum(heaps))
