186f58a0a8ed74eef12a4b7049ce88ee63fcd6e4
kwonhyeokchan <qpjiq123@gmail.com>
2023-02-01 01:33:59 +0900

    15650_N과M(2)

---
+++ baekjoon/15650_N과M(2).java
@@ -0,0 +1,40 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static int n, m;
+    public static boolean[] visit;
+    public static int[] source;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        source = new int[n];
+        for(int i=0; i<n; i++){
+            source[i] = i+1;
+        }
+        visit = new boolean[n];
+        printNums(source, new int[m], visit, 0, m, 0);
+    }
+
+    public static void printNums(int[] source, int[] out, boolean[] visit, int start, int r, int depth){
+        if(depth == r){
+            for(int i=0; i<out.length; i++){
+                System.out.print(out[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=start; i<source.length; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                out[depth] = source[i];
+                printNums(source, out, visit, i+1, r, depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
+
+
